b2ac5036c6dca448bf48e44f2b74c7531c51cb3d
Diego Díaz <didimar2001@gmail.com>
2022-08-18 04:08:48 -0500

    Delete particles ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import Head from 'next/head';
-import { ParticlesContainer } from '../components/particles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,9 +9,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <div className='transition-colors duration-300 grid w-full grid-cols-[1fr,min(640px,100%),1fr] text-black'>
+      <div className='bg-white dark:bg-black transition-colors duration-300 grid w-full grid-cols-[1fr,min(640px,100%),1fr] text-black'>
         <Component {...pageProps} />
-        <ParticlesContainer color={'#0099ff'} />
       </div>
     </ThemeProvider>
   );

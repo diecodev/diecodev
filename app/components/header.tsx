5143c1cef5b1fe2fe80d7217db426046ad45c6e8
Diego Díaz <diegoddiaz05@gmail.com>
2023-01-05 19:09:06 +0000

    Fix: resume.pdf route

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
       <a href='#contact'>
         <li className='flex items-center px-2' >Contact</li>
       </a>
-      <a  href='/resume.pdf' target="_blank" rel="noopener noreferrer">
+      <a  href='/resume' target="_blank" rel="noopener noreferrer">
         <li className='flex items-center px-2 border-2 border-indigo-500 rounded text-indigo-500 hover:bg-indigo-500/20 transition ease-in-out duration-300' >Resume</li>
       </a>
       </ul>

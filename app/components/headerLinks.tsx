b1b13156a11df9e506ff8515449796315959708a
Diego Díaz <diegoddiaz05@gmail.com>
2023-01-06 09:55:48 +0000

    Fix: scroll smooth

--- app/components/headerLinks.tsx
+++ app/components/headerLinks.tsx
@@ -6,13 +6,13 @@ export const HeaderLinks = () => {
   return (
     <>
       <li className='flex items-center px-2' >
-        <Link className='cursor-pointer' to='about' smooth={false} >About</Link>
+        <Link className='cursor-pointer' to='about' smooth={true} offset={-100} >About</Link>
       </li>
       <li className='flex items-center px-2' >
-        <Link className='cursor-pointer' to='experience' smooth={false} >Experience</Link>
+        <Link className='cursor-pointer' to='experience' smooth={true} offset={-100} >Experience</Link>
       </li>
       <li className='flex items-center px-2' >
-        <Link className='cursor-pointer' to='contact' smooth={false} >Contact</Link>
+        <Link className='cursor-pointer' to='contact' smooth={true} offset={-100} >Contact</Link>
       </li>
       <li className='flex items-center px-2 border-2 border-indigo-500 rounded text-indigo-500 hover:bg-indigo-500/20 transition ease-in-out duration-300' >
         <a href='/resume.pdf' target='_blank' rel='noopener noreferrer'>Resume</a>

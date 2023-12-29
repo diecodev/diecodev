ebf4b057c389b6bc47f0c60de594a5e7d9708831
Diecodev <diegoddiaz05@gmail.com>
2023-12-29 01:16:41 -0500

    Remove old files

--- components/footer.tsx
+++
@@ -1,6 +0,0 @@
-export const Footer = () => (
-  <footer class="w-scrren bg-zinc-200 flex flex-col items-center mt-10 py-6">
-    <h4 class="text-xl font-semibold leading-tight">Diecodev</h4>
-    <p class="text-sm">Designed and built by Diego Díaz.</p>
-  </footer>
-);
--- fresh.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "$fresh/server.ts";
-import twindPlugin from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./twind.config.ts";
-
-export default defineConfig({
-  // @ts-expect-error Deno twind config
-  plugins: [twindPlugin(twindConfig)],
-});

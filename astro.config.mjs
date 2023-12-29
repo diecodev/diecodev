92f8f8803e1f0b19f967dab4b6f14ba799f663ac
Diecodev <diegoddiaz05@gmail.com>
2023-12-29 01:57:44 -0500

    Setup deno adaptar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import deno from "@astrojs/deno";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: deno(),
   site: "https://diecodev.deno.dev",
   integrations: [tailwind(), sitemap()],
 });

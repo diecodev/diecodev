b6c790873a345e9cb0bd28d56484f84e26081c8c
Diecodev <diegoddiaz05@gmail.com>
2025-05-04 20:44:15 -0500

    build: 💚 update types to pass ci build

---
+++ mdx.d.ts
@@ -0,0 +1,11 @@
+declare module "*.mdx" {
+	let MDXComponent: (props) => React.ReactNode;
+	export default MDXComponent;
+
+	export const metadata: {
+		title: string;
+		description: string;
+		date: string;
+		tag: string[];
+	};
+}
